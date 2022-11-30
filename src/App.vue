449f8c0a2d0244337d0ceab8e0819e66b01002b7
dsa04156 <dsa04156@naver.com>
2022-11-30 17:53:56 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <main-header />
+  <router-view></router-view>
+</template>
+
+<script>
+import MainHeader from "@/components/MainHeader";
+
+export default {
+  name: "App",
+  components: {
+    MainHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+}
+</style>
